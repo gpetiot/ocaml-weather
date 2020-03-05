ff356344132b99f3f82eb978f191201a8de8f6ca
Guillaume Petiot <guillpetiot@gmail.com>
2020-03-05 22:54:50 +0700

    Add signature for the Lwt backend

---
+++ lib/lwt/weather_lwt.mli
@@ -0,0 +1,48 @@
+module Make (Client : Cohttp_lwt.S.Client) : sig
+  val by_city_state_country :
+    ?lang:Weather_lib.Lang.t ->
+    ?units:Weather_lib.Units.t ->
+    city_name:string ->
+    state:string ->
+    country_code:string ->
+    api_key:string ->
+    (Weather_lib.Response.t, Rresult.R.msg) Result.result Lwt.t
+
+  val by_city_state :
+    ?lang:Weather_lib.Lang.t ->
+    ?units:Weather_lib.Units.t ->
+    city_name:string ->
+    state:string ->
+    api_key:string ->
+    (Weather_lib.Response.t, Rresult.R.msg) Result.result Lwt.t
+
+  val by_city_name :
+    ?lang:Weather_lib.Lang.t ->
+    ?units:Weather_lib.Units.t ->
+    city_name:string ->
+    api_key:string ->
+    (Weather_lib.Response.t, Rresult.R.msg) Result.result Lwt.t
+
+  val by_city_id :
+    ?lang:Weather_lib.Lang.t ->
+    ?units:Weather_lib.Units.t ->
+    city_id:int ->
+    api_key:string ->
+    (Weather_lib.Response.t, Rresult.R.msg) Result.result Lwt.t
+
+  val by_coord :
+    ?lang:Weather_lib.Lang.t ->
+    ?units:Weather_lib.Units.t ->
+    lat:int ->
+    lon:int ->
+    api_key:string ->
+    (Weather_lib.Response.t, Rresult.R.msg) Result.result Lwt.t
+
+  val by_zip_code :
+    ?lang:Weather_lib.Lang.t ->
+    ?units:Weather_lib.Units.t ->
+    ?country_code:string ->
+    zip_code:int ->
+    api_key:string ->
+    (Weather_lib.Response.t, Rresult.R.msg) Result.result Lwt.t
+end
