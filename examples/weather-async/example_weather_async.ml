dedc5e5e705c98dd790182aec173c1c2023cb557
Guillaume Petiot <guillpetiot@gmail.com>
2020-03-16 17:49:38 +0700

    Fix the async example

--- examples/weather-async/example_weather_async.ml
+++ examples/weather-async/example_weather_async.ml
@@ -1,7 +1,19 @@
+open Async
+
 let () =
   let api_key = "your_api_key" in
-  Async_kernel.Deferred.upon
-    (Weather_async.by_city_state ~city_name:"London" ~state:"uk" ~api_key
-       ?lang:None ?units:None) (function
-    | Ok { weather; _ } -> print_endline weather.description
-    | Error (`Msg e) -> print_endline ("error: " ^ e))
+  let logger =
+    let output = [ Log.Output.stdout () ] in
+    Log.create ~level:`Info ~output ~on_error:`Raise
+  in
+  Command.async_spec ~summary:"Sample client" Command.Spec.empty (fun () ->
+      Weather_async.by_city_state ~city_name:"London" ~state:"uk" ~api_key
+        ?lang:None ?units:None
+      >>= function
+      | Ok { weather; _ } ->
+          Log.info logger "%s" weather.description;
+          Deferred.unit
+      | Error (`Msg e) ->
+          Log.error logger "error: %s" e;
+          Deferred.unit)
+  |> Command.run
